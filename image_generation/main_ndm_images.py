0d6914a4cdc870542c1e48972f23e483c3c996ab
Artyom Shulyatev <artyomsh23@gmail.com>
2025-04-19 00:06:15 +0300

    imagesgit status!

---
+++ image_generation/main_ndm_images.py
@@ -0,0 +1,343 @@
+import os
+import wandb
+import torch
+from torch.optim import lr_scheduler
+from torchvision.transforms import Compose, ToTensor, Resize, Normalize, CenterCrop
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import numpy as np
+
+import sys
+sys.path.append("../")
+from ndm import NDM, train_epoch
+from celeba import CelebADataset as CelebA
+from unet_openai.unet import UNetModel
+
+
+class CelebaCustomDataset(CelebA):
+    def __getitem__(self, idx):
+        image, _ = super().__getitem__(idx)
+        return image
+
+def create_celeba_dataloader(config):
+    transform = Compose([Resize(config["image_size"]),
+                         CenterCrop(config["image_size"]),
+                         ToTensor(),
+                         Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+                        )
+    dataset = CelebaCustomDataset(
+        transform=transform,
+        root_dir='/home/tem_shett/porn/CelebA/celeba/celeba',
+    )
+    loader = DataLoader(dataset, config["train_batch_size"], shuffle=True)
+    return loader
+
+def show_images(images, title=""):
+    """Shows the provided images as sub-pictures in a square"""
+
+    # Converting images to CPU numpy arrays
+    if type(images) is torch.Tensor:
+        images = images.detach().cpu().permute(0,2,3,1).numpy()
+
+    # Defining number of rows and columns
+    fig = plt.figure(figsize=(10, 10))
+    rows = int(len(images) ** (1 / 2))
+    cols = round(len(images) / rows)
+
+    # Populating figure with sub-plots
+    idx = 0
+    for r in range(rows):
+        for c in range(cols):
+            fig.add_subplot(rows, cols, idx + 1)
+
+            if idx < len(images):
+                plt.imshow((255*(images[idx]+1)/2).astype('uint8'))
+                idx += 1
+    fig.suptitle(title, fontsize=30)
+
+    # Showing the figure
+    plt.show()
+
+
+def run(config, do_plots=False):
+    # Initialize wandb
+    if config["wandb_logging"]:
+        wandb.init(
+            entity="alexxela12345-hse-university",
+            project="ndm",
+            # name=config["experiment_name"],
+            config=config,
+        )
+
+    device = torch.device(
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+
+    print(f"Using device: {device}")
+
+    dataloader = create_celeba_dataloader(config)
+
+    model = UNetModel(
+        in_channels=3,
+        model_channels=64,
+        out_channels=3,
+        num_res_blocks=2,
+        attention_resolutions=(16, 8),
+        dropout=0.1,
+        channel_mult=(1, 2, 4),
+        conv_resample=True,
+        dims=2,
+        num_classes=None,
+        use_checkpoint=False,
+        num_heads=1,
+        num_heads_upsample=-1,
+        use_scale_shift_norm=False,
+    )
+
+    print(f'UNetModel numer of parameters: {sum([p.numel() for p in model.parameters()])}')
+
+    model_F = UNetModel(
+        in_channels=3,
+        model_channels=64,
+        out_channels=3,
+        num_res_blocks=2,
+        attention_resolutions=(16, 8),
+        dropout=0.1,
+        channel_mult=(1, 2, 4),
+        conv_resample=True,
+        dims=2,
+        num_classes=None,
+        use_checkpoint=False,
+        num_heads=1,
+        num_heads_upsample=-1,
+        use_scale_shift_norm=False,
+    )
+
+    # Load pretrained model if specified
+    if config["load_pretrained"]:
+        assert not config.get("was_learnable", False) or config["schedule_config"].get("learnable",
+                                                                                       False)
+        need_switch = config.get("was_learnable", False) ^ config["schedule_config"].get(
+            "learnable", False)
+        config["schedule_config"]["learnable"] = config["was_learnable"]
+        ndm = NDM(
+            model=model,
+            model_F=model_F,
+            schedule_config=config["schedule_config"],
+            num_timesteps=config["num_timesteps"],
+            importance_sampling_batch_size=config["importance_sampling_batch_size"],
+            uniform_prob=config["uniform_prob"],
+            predict_noise=config["predict_noise"],
+            ddim_sampling=config["ddim_sampling"]
+        ).to(device)
+        if config["pretrained_model_path"] is None:
+            raise ValueError(
+                "pretrained_model_path must be specified when load_pretrained is True"
+            )
+        if config["pretrained_run_id"]:
+            print(f"Loading model from wandb run {config['pretrained_run_id']}")
+            model_path = wandb.restore(
+                config["pretrained_model_path"],
+                run_path=f"{config['pretrained_run_id']}",
+            )
+            print(ndm.alphas_cumprod)
+            print(model_path.name)
+            ndm.load_state_dict(torch.load(model_path.name, map_location=device))
+        elif config["pretrained_model_path"]:
+            print(f"Loading model from local path {config['pretrained_model_path']}")
+            ndm.load_state_dict(
+                torch.load(config["pretrained_model_path"], map_location=device)
+            )
+        else:
+            raise ValueError(
+                "Either pretrained_run_id or pretrained_model_path must be specified when load_pretrained is True"
+            )
+        if need_switch:
+            ndm.alphas_cumprod = nn.Parameter(ndm.alphas_cumprod, requires_grad=True)
+            config["schedule_config"]["learnable"] = True
+        print('here', need_switch, ndm.alphas_cumprod)
+    else:
+        ndm = NDM(
+            model=model,
+            model_F=model_F,
+            schedule_config=config["schedule_config"],
+            num_timesteps=config["num_timesteps"],
+            importance_sampling_batch_size=config["importance_sampling_batch_size"],
+            uniform_prob=config["uniform_prob"],
+            predict_noise=config["predict_noise"],
+            ddim_sampling=config["ddim_sampling"]
+        ).to(device)
+
+    if config["wandb_logging"]:
+        wandb.watch(ndm, log_freq=100)
+
+    # Initialize optimizer based on config
+    optimizer_parameters = list(ndm.model.parameters()) + list(ndm.model_F.parameters())
+    if config["schedule_config"].get("learnable", False):
+        optimizer_parameters += [ndm.alphas_cumprod]
+    if config["optimizer_type"].lower() == "sgd":
+        optimizer = torch.optim.SGD(
+            optimizer_parameters,
+            lr=config["learning_rate"],
+            momentum=config["momentum"],
+            weight_decay=config["weight_decay"],
+        )
+    elif config["optimizer_type"].lower() == "adamw":
+        optimizer = torch.optim.AdamW(
+            optimizer_parameters,
+            lr=config["learning_rate"],
+            weight_decay=config["weight_decay"],
+        )
+    else:
+        raise ValueError(f"Unsupported optimizer type: {config['optimizer_type']}")
+
+    # Calculate total steps and create scheduler
+    total_training_steps = len(dataloader) * config["num_epochs"]
+
+    def lr_lambda(current_step):
+        if current_step < config["warmup_steps"]:
+            return float(current_step) / float(max(1, config["warmup_steps"]))
+
+        decay_factor = max(
+            0.0,
+            float(total_training_steps - current_step) / float(
+                max(1, total_training_steps - config["warmup_steps"]))
+        )
+        return decay_factor * (1.0 - config["final_lr"] / config["learning_rate"]) + config[
+            "final_lr"] / config["learning_rate"]
+
+    # Using LambdaLR for more explicit linear decay control
+    scheduler = lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
+
+    global_step = 0
+    frames = []
+    losses = []
+    epoch_losses = []
+    print("Training model...")
+    for epoch in range(config["num_epochs"]):
+        print(f"Epoch {epoch}")
+        global_step, cur_losses = train_epoch(
+            ndm,
+            dataloader,
+            optimizer,
+            scheduler,
+            global_step,
+            config["gradient_clipping"],
+        )
+        if config["schedule_config"].get("learnable", False):
+            print("alphas_cumprod after train_epoch:", ndm.alphas_cumprod)
+        losses.extend(cur_losses)
+        epoch_loss = np.mean(cur_losses)
+        epoch_losses.append(epoch_loss)
+        print(f"Epoch {epoch} finished with loss: {epoch_loss}")
+
+        # Log metrics to wandb
+        if config["wandb_logging"]:
+            wandb.log(
+                {
+                    "epoch": epoch + config["pretrained_epochs"],
+                    "loss": epoch_loss,
+                    "learning_rate": scheduler.get_last_lr()[0],
+                },
+                step=epoch + config["pretrained_epochs"],
+            )
+
+        if epoch % config["save_images_step"] == 0 or epoch == config["num_epochs"] - 1:
+            # generate data with the model to later visualize the learning process
+            sample = ndm.sample(config["eval_batch_size"], (3, config["image_size"], config["image_size"]))
+            # frames.append(sample.cpu().numpy())
+
+            if do_plots:
+                plt.figure(figsize=(16, 8))
+
+                plt.subplot(1, 2, 1)
+                plt.title("Loss per step")
+                plt.plot(list(range(len(losses))), losses)
+                plt.yscale("log")
+
+                plt.subplot(1, 2, 2)
+                plt.title("Loss per epoch")
+                plt.plot(list(range(len(epoch_losses))), epoch_losses)
+                plt.yscale("log")
+
+                # Log the figure to wandb
+                # if config["wandb_logging"]:
+                #     wandb.log(
+                #         {"training_visualization": wandb.Image(plt)},
+                #         step=epoch + config["pretrained_epochs"],
+                #     )
+                plt.show(block=False)
+                plt.pause(0.1)
+                plt.close()
+
+                show_images(sample)
+
+    # print("Saving model...")
+    # outdir = f"exps/{config['experiment_name']}"
+    # os.makedirs(outdir, exist_ok=True)
+    # torch.save(ndm.state_dict(), f"{outdir}/model.pth")
+    # save model to wandb
+    # if config["wandb_logging"]:
+    #     wandb.save(f"{outdir}/model.pth")
+    #
+    # print("Saving images...")
+    # imgdir = f"{outdir}/images"
+    # os.makedirs(imgdir, exist_ok=True)
+    # frames = np.stack(frames)
+    # xmin, xmax = -6, 6
+    # ymin, ymax = -6, 6
+    # for i, frame in enumerate(frames):
+    #     plt.figure(figsize=(10, 10))
+    #     plt.scatter(frame[:, 0], frame[:, 1])
+    #     plt.xlim(xmin, xmax)
+    #     plt.ylim(ymin, ymax)
+    #     plt.savefig(f"{imgdir}/{i:04}.png")
+    #     plt.close()
+    # print("Saving loss as numpy array...")
+    # np.save(f"{outdir}/loss.npy", np.array(losses))
+    #
+    # print("Saving frames...")
+    # np.save(f"{outdir}/frames.npy", frames)
+    #
+    # Finish wandb run
+    # if config["wandb_logging"]:
+    #     wandb.finish()
+#
+
+
+if __name__ == "__main__":
+    config = {
+        "experiment_name": "ndm_images_1000steps",
+        "wandb_logging": False,
+        "image_size": 16,
+        "train_batch_size": 64,
+        "eval_batch_size": 16,
+        "num_epochs": 1000,
+        "learning_rate": 3e-4,
+        "warmup_steps": 0,
+        "num_timesteps": 1000,
+        "schedule_config": {"type": "cosine", "min_alpha": 0.0001, "max_alpha": 0.9999,
+                            "learnable": False},
+        # "schedule_config": {"type": "linear", "beta_start": 0.0001, "beta_end": 0.02, "learnable": False},
+        "save_images_step": 1,
+        "gradient_clipping": None,
+        "importance_sampling_batch_size": None,
+        "uniform_prob": 0.001,
+        "optimizer_type": "adamw",
+        "momentum": 0.9,
+        "weight_decay": 0.00001,
+        # New parameters for model loading
+        "load_pretrained": False,
+        # "pretrained_run_id": "ndm/esjakfmk",  # wandb run ID to load model from
+        "pretrained_run_id": None,
+        # "pretrained_model_path": "exps/ndm_1000steps/model.pth",  # local path to load model from (alternative to wandb)
+        "pretrained_model_path": None,
+        "pretrained_epochs": 0,  # number of epochs the pretrained model was trained for
+        "predict_noise": True,
+        "ddim_sampling": False,
+        "final_lr": 1e-10
+    }
+    run(config, do_plots=True)
--- ndm.py
+++ ndm.py
@@ -66,15 +66,15 @@ class NDM(nn.Module):
             assert (0 <= t).all() and (t <= self.num_timesteps).all()
         else:
             assert 0 <= t and t <= self.num_timesteps
-        t_scaled = t.view(-1, 1) / self.num_timesteps
+        t_scaled = t.view(-1, *([1] * (x.dim() - 1))) / self.num_timesteps
         return (1 - t_scaled) * x + t_scaled * self.model_F(x, t)
 
     def reconstruct_x0(self, z_t, t, noise):
         assert self.predict_noise
         s1 = torch.sqrt(1 / self.alphas_cumprod[t])
         s2 = torch.sqrt(1 / self.alphas_cumprod[t] - 1)
-        s1 = s1.reshape(-1, 1)
-        s2 = s2.reshape(-1, 1)
+        s1 = s1.reshape(-1, *([1] * (z_t.dim() - 1)))
+        s2 = s2.reshape(-1, *([1] * (z_t.dim() - 1)))
         return s1 * z_t - s2 * noise
 
     def q_posterior(self, x_0, z_t, t):
@@ -97,9 +97,9 @@ class NDM(nn.Module):
         s1 = -torch.sqrt(sigma_s**2 - sigma_st**2) / sigma_t * torch.sqrt(a_t)
         s2 = torch.sqrt(sigma_s**2 - sigma_st**2) / sigma_t
 
-        s0 = s0.reshape(-1, 1)
-        s1 = s1.reshape(-1, 1)
-        s2 = s2.reshape(-1, 1)
+        s0 = s0.reshape(-1, *([1] * (x_0.dim() - 1)))
+        s1 = s1.reshape(-1, *([1] * (x_0.dim() - 1)))
+        s2 = s2.reshape(-1, *([1] * (x_0.dim() - 1)))
 
         mu = (
             s0 * self.F(x_0, (t - 1).repeat(x_0.shape[0]))
@@ -140,17 +140,17 @@ class NDM(nn.Module):
         s1 = torch.sqrt(self.alphas_cumprod[timesteps])
         s2 = torch.sqrt(1 - self.alphas_cumprod[timesteps])
 
-        s1 = s1.reshape(-1, 1)
-        s2 = s2.reshape(-1, 1)
+        s1 = s1.reshape(-1, *([1] * (x_start.dim() - 1)))
+        s2 = s2.reshape(-1, *([1] * (x_start.dim() - 1)))
 
         return s1 * self.F(x_start, timesteps) + s2 * x_noise
 
     @torch.no_grad()
-    def sample(self, n_samples):
+    def sample(self, n_samples, sample_size=(2,)):
         self.model.eval()
         self.model_F.eval()
         device = next(self.parameters()).device
-        sample = torch.randn(n_samples, 2, device=device)
+        sample = torch.randn(n_samples, *sample_size, device=device)
         timesteps = list(range(1, self.num_timesteps + 1))[::-1]
         for t in tqdm(timesteps):
             t = torch.from_numpy(np.repeat(t, n_samples)).long().to(device)
@@ -177,8 +177,11 @@ def train_epoch(
         with torch.no_grad():
             if ndm.alphas_cumprod[0] > 0.9999:
                 ndm.alphas_cumprod[0] = 0.9999
-        batch = batch[0].to(device)
-        noise = torch.randn(batch.shape, device=device)
+        if type(batch) == list: # 2d-dataset
+            batch = batch[0].to(device)
+        else:
+            batch = batch.to(device)
+        noise = torch.randn_like(batch)
 
         if (
             importance_sampling
@@ -229,14 +232,14 @@ def train_epoch(
             xhat = noise_pred
         # print(xhat.min(), xhat.max())
 
-        coef_1 = torch.sqrt(a_s).view(-1, 1)
+        coef_1 = torch.sqrt(a_s).view(-1, *([1] * (batch.dim() - 1)))
         loss_1 = coef_1 * (ndm.F(batch, timesteps - 1) - ndm.F(xhat, timesteps - 1))
 
         coef_2 = (
             torch.sqrt(sigma_s**2 - sigmast_squared)
             / sigma_t
             * torch.sqrt(a_t)
-        ).view(-1, 1)
+        ).view(-1, *([1] * (batch.dim() - 1)))
         loss_2 = coef_2 * (ndm.F(xhat, timesteps) - ndm.F(batch, timesteps))
 
         loss = loss_1 + loss_2
